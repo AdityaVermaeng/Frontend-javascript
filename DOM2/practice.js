ebc4d6a260a92d20fbc2ad588c67df2339af9deb
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-09-21 12:30:59 +0530

    first-commit

---
+++ DOM2/practice.js
@@ -0,0 +1,12 @@
+// let body = document.querySelector("body")
+// console.log(body);l=
+let el = document.createElement("button");
+el.innerText = "click-me";
+el.style.backgroundColor = "red";
+el.style.color = "white";
+let newBtn = document.querySelector("body");
+newBtn.prepend(el);
+
+//Q2
+let para = document.querySelector("p");
+para.classList.add("content2")
---
+++ DOM2/script.js
@@ -0,0 +1,21 @@
+// let div = document.querySelector("div")
+// console.log(div)
+// let id = div.getAttribute("id")
+// console.log(id);
+// let name = div.getAttribute("name")
+// console.log(name);
+// let para = document.querySelector("p")
+// console.log(para.setAttribute("class", "newclass"))
+// let div = document.querySelector("div")
+// console.log(div)
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "click-me"
+// console.log(newBtn);
+// let div = document.querySelector("div");
+// div.append(newBtn);
+// div.prepend(newBtn);
+// div.after(newBtn);
+// div.before(newBtn)
+let p = document.querySelector("p");
+p.remove();
+// p.after(newBtn)
