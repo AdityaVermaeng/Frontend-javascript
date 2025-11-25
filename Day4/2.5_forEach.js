411558d4ed9c181b2f4a2f0b1143fb1c03a9fae3
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-11-25 23:02:21 +0530

    user

--- Day4/2.5_forEach.js
+++ Day4/2.5_forEach.js
@@ -3,3 +3,30 @@ const fruits = ["MAngo", "Banana", "orange", "Papaya"];
 fruits.forEach((fruits, index) => {
     console.log(`Fruit at index ${index} is ${fruits}`);
 })
+
+//
+
+let user = [{
+        name: "Aditya",
+        age: 34
+    },
+    {
+        name: "Amit",
+        age: 18
+    },
+    {
+        name: "Anubhav",
+        age: 17
+    },
+    {
+        name: "Dhananjay",
+        age: 12
+    },
+    {
+        name: "gagan",
+        age: 54
+    }
+]
+user.forEach((obj, inx) => {
+    console.log(inx, obj.name, obj.age);
+})
