b4dc2201d4b3007dbcecd8dd4f7ffbc43f3b0e4d
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-11-25 16:04:18 +0530

    task-3

--- Day4/3_tasks.js
+++ Day4/3_tasks.js
@@ -14,3 +14,28 @@ let checkVowels = (str) => {
 console.log(checkVowels("welcometohomeaeiou"))
 
 //task2- factorial of a number
+
+let factorial = (x) => {
+    let fact = 1;
+    for (let i = 1; i <= x; i++) {
+        fact *= i;
+    }
+    return fact;
+
+}
+console.log(factorial(5))
+
+//check the word count in a sentence
+function wordCount(str) {
+    // return str.split("").length
+    let count = 1;
+
+    for (v of str) {
+        if (v == " ")
+        // console.log(v)
+            count++;
+    }
+    return count
+}
+console.log(wordCount("welcome to my site guyz!"));
+console.log(wordCount("welcome to my site guyz! task 3"));
