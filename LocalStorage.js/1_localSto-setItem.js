b45466e063f6badafde28f037ac88de4eb233b4c
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-11-28 14:26:32 +0530

    local storage

---
+++ LocalStorage.js/1_localSto-setItem.js
@@ -0,0 +1,23 @@
+// localStorage.setItem("userName", "Aadi")
+// console.log(localStorage.getItem('userName'));
+let bodyElement = document.querySelector("body");
+let allBtns = document.querySelectorAll("button");
+allBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        localStorage.setItem("ColorTheme", btn.innerHTML)
+        localStorage.setItem("TIME", new Date().toLocaleTimeString()) //we want to remove this key from our local storage
+        setColorThemeBody()
+
+        // console.log();
+    })
+})
+
+let setColorThemeBody = () => {
+    let color = localStorage.getItem('ColorTheme')
+    bodyElement.style.background = color
+
+}
+setColorThemeBody() //only when refresh it will execute
+    // localStorage.removeItem("TIME") //it remove the "TIME "key from local storage
+
+// localStorage.clear() //it clear the all data from our local storage
