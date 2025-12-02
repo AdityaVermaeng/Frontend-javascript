2dcb1b14c4cec4744e5360745c097d2640e2d911
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-02 11:11:39 +0530

    FetchGet-Method

---
+++ promises/fetchGet.js
@@ -0,0 +1,24 @@
+// fetch("http://jsonplaceholder.typicode.com/posts")
+//     .then(function(response) {
+//         return response.json(); //convert response to json
+//     })
+//     .then(function(data) {
+//         console.log("GET Response:");
+//         console.log(data); //print data
+//     }).catch(function(error) {
+//         console.log("Error:", error); //network error
+//     })
+
+//fetch data from text file
+
+fetch("./fetchdata.txt")
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(data) {
+        console.log("GET Response:");
+        console.log(data);
+    })
+    .catch(function(error) {
+        console.log("Error:", error);
+    })
