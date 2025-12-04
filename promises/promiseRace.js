77afacd8cae137271f86aa62b7cfac3971d485cd
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-04 15:33:18 +0530

    promise.race

--- promises/promiseRace.js
+++ promises/promiseRace.js
@@ -1,11 +1,21 @@
-function wait(ms, value) {
-    return new Promise(resolve => setTimeout(() => resolve(value), ms))
-}
-const p1 = wait(300, "A"); //resolve after 300ms
-const p2 = wait(100, "B"); //resolve after 100ms
-const p3 = wait(200, "C"); //resolve after 200ms
-Promise.race([p1, p2, p3]).then(results => {
-    console.log("Race resolved with:", results); //result B
-}).catch(error => {
-    console.log("Race rejcted with:", error);
-})
+// function wait(ms, value) {
+//     return new Promise(resolve => setTimeout(() => resolve(value), ms))
+// }
+// const p1 = wait(300, "A"); //resolve after 300ms
+// const p2 = wait(100, "B"); //resolve after 100ms
+// const p3 = wait(200, "C"); //resolve after 200ms
+// Promise.race([p1, p2, p3]).then(results => {
+//     console.log("Race resolved with:", results); //result B
+// }).catch(error => {
+//     console.log("Race rejcted with:", error);
+// })
+
+//
+const p1 = new Promise((resolve) => setTimeout(() => resolve("OK"), 300));
+const p2 = new Promise((resolve) => setTimeout(() => resolve("Race Resolved with:"), 100));
+const p3 = new Promise((resolve) => setTimeout(() => resolve("Ok3"), 200))
+Promise.race([p1, p2, p3])
+    .then(results => console.log(results))
+    .catch(err => console.error("Error is:", err));
+
+//
