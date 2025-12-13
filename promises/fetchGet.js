d8adf55607983e5bca2f27010bb4595188aaa916
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-13 23:27:05 +0530

    Get method

--- promises/fetchGet.js
+++ promises/fetchGet.js
@@ -1,13 +1,13 @@
-// fetch("://jsonplachttpeholder.typicode.com/posts")
-//     .then(function(response) {
-//         return response.json(); //convert response to json
-//     })
-//     .then(function(data) {
-//         console.log("GET Response:");
-//         console.log(data); //print data
-//     }).catch(function(error) {
-//         console.log("Error:", error); //network error
-//     })
+fetch("://jsonplachttpeholder.typicode.com/posts")
+    .then(function(response) {
+        return response.json(); //convert response to json
+    })
+    .then(function(data) {
+        console.log("GET Response:");
+        console.log(data); //print data
+    }).catch(function(error) {
+        console.log("Error:", error); //network error
+    })
 
 //fetch data from text file
 
