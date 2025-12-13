70532d131f99268a3f0dc8731d5ddf958832246f
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-13 23:35:48 +0530

    setTimeout()

--- promises/setTimeOut.js
+++ promises/setTimeOut.js
@@ -1,3 +1,14 @@
 setTimeout(() => {
     console.log("Hello Students")
 }, 4000)
+
+
+//timeout
+function timeOut(value, ms) {
+    return new Promise(res => setTimeout(() => res(value), timeout))
+}
+const q1 = wait(300, "A"); //resolve after 300ms
+const q2 = wait(100, "B"); //resolve after 100ms
+const q3 = wait(200, "C"); //resolve after 200ms
+Promise.race([q1, q2, q3]).then(r => console.log("Here are the result:", r))
+    .catch(err => console.log("ERROR IS:", err))
