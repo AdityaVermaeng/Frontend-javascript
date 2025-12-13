6fbbf062ed75a1dff8fad3b804bd6220b179d7ec
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-13 23:28:26 +0530

    PromiseRace()

--- promises/promiseRace.js
+++ promises/promiseRace.js
@@ -1,14 +1,14 @@
-// function wait(ms, value) {
-//     return new Promise(resolve => setTimeout(() => resolve(value), ms))
-// }
-// const p1 = wait(300, "A"); //resolve after 300ms
-// const p2 = wait(100, "B"); //resolve after 100ms
-// const p3 = wait(200, "C"); //resolve after 200ms
-// Promise.race([p1, p2, p3]).then(results => {
-//     console.log("Race resolved with:", results); //result B
-// }).catch(error => {
-//     console.log("Race rejcted with:", error);
-// })
+function wait(ms, value) {
+    return new Promise(resolve => setTimeout(() => resolve(value), ms))
+}
+const q1 = wait(300, "A"); //resolve after 300ms
+const q2 = wait(100, "B"); //resolve after 100ms
+const q3 = wait(200, "C"); //resolve after 200ms
+Promise.race([q1, q2, q3]).then(results => {
+    console.log("Race resolved with:", results); //result B
+}).catch(error => {
+    console.log("Race rejcted with:", error);
+})
 
 //
 const p1 = new Promise((resolve) => setTimeout(() => resolve("OK"), 300));
