227e36ee2ad5533f60def1f83f2643acdce16867
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-17 11:44:34 +0530

    DoM

--- DOM1.1/0_select.js
+++ DOM1.1/0_select.js
@@ -1,5 +1,22 @@
-function change() {
-    let ele = document.getElementById("h1")
-    ele.innerText = "heyy my name is Aditya";
-    ele.style.backgroundColor = "Green"
-}
+// function change() {
+//     let ele = document.getElementsByClassName("h1")
+//     ele.innerText = "heyy my name is Aditya";
+//     ele.style.backgroundColor = "Green"
+// }
+
+
+
+
+// console.log(document);
+// console.log(document.all);
+// console.log(document.all[0]);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.URL);
+// console.log(document.baseURL);
+// console.log(document.domain);
+// console.log(document.doctype);
+console.log(document.scripts);
--- DOM1.1/1_selector.js
+++ DOM1.1/1_selector.js
@@ -9,18 +9,18 @@ el.innerHTML = "<b>Hello DOM</b>";
 console.log(el);
 
 //getElementbyClassName
-let heading = document.getElementsByClassName("head")[0];
+// let heading = document.getElementsByClassName("head")[0];
 // console.log(heading); // heading is an Array like object  
 // console.log(heading.innerHTML);
 // console.log(heading.innerText);
 
 //getElementbyTagName
 
-let bold = document.getElementsByTagName("b")[0];
-console.log(bold.innerText);
+// let bold = document.getElementsByTagName("b")[0];
+// console.log(bold.innerText);
 
 //querrySelector
-let div = document.querySelector(".card");
+// let div = document.querySelector(".card");
 // console.log(div);
 // console.log(div.innerHTML);
 // console.log(div.innerText);
