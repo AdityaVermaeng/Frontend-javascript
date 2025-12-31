fe7fc879153bbd70df752ac9e4214bd13a4c1107
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-31 10:28:26 +0530

    feat(code):add constructor function program

---
+++ OOPS/constructor_function.js
@@ -0,0 +1,12 @@
+function Student(name1, age, address) {
+    this.name1 = name1
+    this.age = age
+    this.address = address
+    getInfo = function() {
+        console.log(`My name is ${this.myName}. I am ${this.age}. I am from${this.address}`)
+    }
+}
+obj1 = new student("Aditya", 21, "Noida");
+obj2 = new student("Anshu", 21, "Noida18")
+obj1.getInfo();
+obj2.getInfo();
