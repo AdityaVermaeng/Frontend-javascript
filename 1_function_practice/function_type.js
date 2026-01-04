a6bd4118b0bb72d80d1afd61acccd6fe6d9cb6da
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-01-04 23:57:04 +0530

    feat:add some more function type

--- 1_function_practice/function_type.js
+++ 1_function_practice/function_type.js
@@ -7,6 +7,17 @@ console.log(multiply(4));
 
 
 
-//Arrow function
+//Arrow function(ES6)
 let add = (p, q) => p + q;
-console.log(add(3, 9))
+console.log(add(3, 9));
+
+//Annonymous function(function without name)
+setTimeout(function() {
+    console.log("Hello");
+}, 1000);
+
+//Named function expression(expression with function name)
+const greet = function hello() {
+    console.log("Hi");
+};
+greet();
