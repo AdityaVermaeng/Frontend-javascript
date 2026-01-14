3fe7ac1e22082fa1f2f5e8dfe40b9d36ea603852
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-01-14 11:25:34 +0530

    feat: add a smartphone innformation question

--- OOPS/question.js
+++ OOPS/question.js
@@ -31,8 +31,32 @@ class BankAcoount {
 }
 let aadi = new BankAcoount("Aditya", 1000);
 // console.log(aadi.balance);
-aadi.deposite(99);
+// aadi.deposite(99);
 // console.log("Amount deposite successfully", aadi.balance);
-aadi.checkBalance();
-aadi.withdraw(1050);
-aadi.withdraw(50);
+// aadi.checkBalance();
+// aadi.withdraw(1050);
+// aadi.withdraw(50);
+
+
+//create a class of a SmartPhone (Name,color,price,RAM,Camera,battery,charging)
+
+class SmartPhone {
+    constructor(name, color, price, RAM, camera, battery, charging) {
+        this.brand = name;
+        this.color = color;
+        this.price = price;
+        this.ram = RAM;
+        this.camera = camera;
+        this.battery = battery;
+        this.charging = charging;
+
+    }
+    getInfo() {
+        console.log(`brand name is ${this.brand},color is ${this.color},which price is ${this.price}`);
+        console.log(`comes with${this.ram}GB RAM,${this.camera}megaPixel and ${this.battery}mAh massive Battery with ${this.charging}watt superfast charger`)
+    }
+}
+let samsung = new SmartPhone("Samsung", "silver", 20500, 8, 32, 4800, 85);
+let realme = new SmartPhone("Realme", "black", 24500, 12, 52, 5800, 115);
+samsung.getInfo();
+realme.getInfo();
