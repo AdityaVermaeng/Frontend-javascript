4d1003c345b3d8ce65ecf4e2ca554d58dc4d9ac2
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-02-07 23:58:23 +0530

    feat: add callback hell with description

--- promises/callback.js
+++ promises/callback.js
@@ -13,18 +13,18 @@ console.log("task3 ");
 
 //callback hell //nested callbacks
 setTimeout(() => {
-        console.log("task2 ");
+    console.log("task2 ");
+    setTimeout(() => {
+        console.log("task3 ");
         setTimeout(() => {
-            console.log("task3 ");
+            console.log("task1");
             setTimeout(() => {
-                console.log("task1");
-                setTimeout(() => {
-                    console.log("task0");
-                }, 4000)
-            }, 5000)
-        }, 2000)
+                console.log("task0");
+            }, 4000)
+        }, 5000)
+    }, 2000)
 
-    }, 3000) //before the promise we use this callback hell
+}, 3000); //before the promise we use this callback hell
 
 // callback hell or pyramid of DOOM
 
@@ -37,9 +37,9 @@ api.CreateOrder(cart, function() {
             api.ShowOrderSummury(
                 function() {
                     api.UpdateWallet()
-                })
-        })
-})
+                });
+        });
+});
 
 
 
