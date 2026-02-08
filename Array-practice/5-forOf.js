9e1d846aa27302aaf0b3fa3365355b312c936961
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-02-08 22:16:08 +0530

    feat:add sum of array using for of loop

---
+++ Array-practice/5-forOf.js
@@ -0,0 +1,14 @@
+// let arr = [10, 20, 30, 40];
+// for (let item of arr) {
+//     console.log(item);
+//     // console.log(lengthOf(item));
+// }
+
+
+//sum of array using for of
+let arr1 = [20, 40, 54, 33, 23, 25];
+var sum = 0;
+for (let num of arr1) {
+    sum += num;
+}
+console.log(sum);
