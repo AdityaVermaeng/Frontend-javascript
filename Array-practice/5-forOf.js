0b45129db9e4a97bb0137e1e5fa6a1bbe461d3d0
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-02-08 22:20:32 +0530

    feat:add largest val  of array using for of loop

--- Array-practice/5-forOf.js
+++ Array-practice/5-forOf.js
@@ -12,3 +12,14 @@ for (let num of arr1) {
     sum += num;
 }
 console.log(sum);
+
+
+// find largest number
+let arr2 = [20, 40, 54, 33, 23, 25];
+let max = arr2[0];
+for (let val of arr2) {
+    if (val > max) {
+        max = val;
+    }
+}
+console.log(max);
